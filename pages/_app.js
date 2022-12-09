4d881b401d0c5f62c6d71c6857920b2b13423606
Arya Krishna <dr.aryakris@outlook.com>
2022-12-09 09:44:30 -0800

    Add files via upload

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+// The default export of _app.js is a top-level React component that wraps all the pages in your application. You can use this component to keep state when navigating between pages, or to add global styles as we're doing here. 
+
+//In Next.js, you can add global CSS files by importing them from pages/_app.js. You cannot import global CSS anywhere else.
+
+
+
+export default function App({ Component, pageProps }) {
+    return <Component {...pageProps} />;
+}
